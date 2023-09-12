b0ce0e7d38fae48b830dd463011af912fba3d663
Obimbo07 <obimboausts@gmail.com>
2023-09-12 04:22:48 +0300

    'banner-changes'

---
+++ main.js
@@ -0,0 +1,27 @@
+
+    const headerText = "Welcome to Barbie's Cyber Security hub";
+    const headerElement = document.getElementById("typed-header");
+    let currentIndex = 0;
+    let isDeleting = false;
+
+    function typeText() {
+        if (isDeleting) {
+            headerElement.textContent = headerText.substring(0, currentIndex - 1);
+            currentIndex--;
+        } else {
+            headerElement.textContent = headerText.substring(0, currentIndex + 1);
+            currentIndex++;
+        }
+
+        if (currentIndex > headerText.length) {
+            isDeleting = true;
+        }
+
+        if (currentIndex === 0) {
+            isDeleting = false;
+        }
+    }
+
+    // Adjust the interval (e.g., 100 milliseconds) for typing speed.
+    const typingInterval = setInterval(typeText, 100);
+
